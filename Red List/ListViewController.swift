7507713b285d2718a9223024502d35098935785f
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-01-27 16:20:18 -0500

    Set up Tableview Datasource Methods

--- Red List/ListViewController.swift
+++ Red List/ListViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ListViewController: UITableViewController {
 
+    let itemArray = ["Buy Milk for Benny", "Promote Synergy", "Throw out old bandaids"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
 
+    
