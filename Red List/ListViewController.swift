3bfee5efa71b7f6924ef2207755a6c9fc4673fe4
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-01-27 16:59:14 -0500

    added tableview functionality for row selection, also added the checkmark functionality

--- Red List/ListViewController.swift
+++ Red List/ListViewController.swift
@@ -20,11 +20,24 @@ class ListViewController: UITableViewController {
         return itemArray.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+ 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
     
