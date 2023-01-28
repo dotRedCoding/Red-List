4663a90b73dda3c560b055c520c51a1863d03d4f
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-01-28 16:52:16 -0500

    Items Array now saved into a .plist that is Codable. Added saveItem and loadItem App no longer needs a hardcoded array to test

--- Red List/Controller/ListViewController.swift
+++ Red List/Controller/ListViewController.swift
@@ -11,17 +11,13 @@ class ListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        let newItem = Item()
-        newItem.title = "Buy Milk"
-        itemArray.append(newItem)
         
-       /* if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }*/
+        loadItems()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,7 +39,7 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -58,13 +54,12 @@ class ListViewController: UITableViewController {
             //what happens once the user clicks the Add Item button
             
             let newItem = Item()
-            
             newItem.title = typedText.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
             
+            self.saveItems()
         }
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "What would you like to add?"
             typedText = alertTextField
@@ -73,7 +68,36 @@ class ListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+    }
     
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+            
+        }
+        
+    }
 }
+
+
 
 
