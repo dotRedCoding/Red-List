2f342d48f15c3bb09e87149bb71638879530105e
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-01-28 11:44:59 -0500

    New items can now be added to the array and they will be saved with persistent data

--- Red List/ListViewController.swift
+++ Red List/ListViewController.swift
@@ -8,26 +8,31 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-
-    let itemArray = ["Buy Milk for Benny", "Promote Synergy", "Throw out old bandaids"]
+    
+    var itemArray = ["Buy Milk for Benny", "Promote Synergy", "Throw out old bandaids"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
- 
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-     
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
             
@@ -38,6 +43,29 @@ class ListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var typedText = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Red List Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default)
+        { (action) in
+            //what happens once the user clicks the Add Item button
+            self.itemArray.append(typedText.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "What would you like to add?"
+            typedText = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
-    
+
