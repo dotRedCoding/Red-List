bc1631864f366ad70eaa6060a619f3d6333a7f25
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-01-28 12:40:23 -0500

    Little bit of code cleanup before restructuring for MVC

--- Red List/Controller/ListViewController.swift
+++ Red List/Controller/ListViewController.swift
@@ -9,16 +9,19 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-    var itemArray = ["Buy Milk for Benny", "Promote Synergy", "Throw out old bandaids"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let newItem = Item()
+        newItem.title = "Buy Milk"
+        itemArray.append(newItem)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+       /* if let items = defaults.array(forKey: "TodoListArray") as? [String] {
             itemArray = items
-        }
+        }*/
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,19 +30,20 @@ class ListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        //Ternary can also be written - cell.accessoryType = item.done ? .checkmark : .none
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -52,7 +56,11 @@ class ListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default)
         { (action) in
             //what happens once the user clicks the Add Item button
-            self.itemArray.append(typedText.text!)
+            
+            let newItem = Item()
+            
+            newItem.title = typedText.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
             
---
+++ Red List/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  List.swift
+//  Red List
+//
+//  Created by Jared on 2023-01-28.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
